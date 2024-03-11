e00f2e2eccdbb5cae81905dd0164506d6a7efb60
Yevgeny Bakalenko <yevgeny.bakalenko@gmail.com>
2024-03-11 08:16:38 +0300

    my first ansible commit

---
+++ config-scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+APP_DIR=${1:-$HOME}
+sudo apt update
+sudo apt-get install -y git
+git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+cd $APP_DIR/reddit
+bundle install
+sudo puma -d
+#sudo mv ./puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
